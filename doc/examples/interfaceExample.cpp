e7308dee91299de7fce245c769653c556657bcc5
Marc G. Bellemare <marcgb@gmail.com>
2013-04-19 11:59:22 -0600

    Added some documentation

---
+++ doc/examples/interfaceExample.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <ale_interface.hpp>
+using namespace std;
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " rom_file" << std::endl;
+        return 1;
+    }
+
+    ALEInterface ale;
+
+    // Load the ROM file
+    ale.loadROM(argv[1], false);
+
+    // Get the vector of legal actions
+    ActionVect legal_actions = ale.getLegalActionSet();
+
+    // Play 10 episodes
+    for (int episode=0; episode<10; episode++) {
+        float totalReward = 0;
+        while (!ale.game_over()) {
+            Action a = legal_actions[rand() % legal_actions.size()];
+            // Apply the action and get the resulting reward
+            float reward = ale.act(a);
+            totalReward += reward;
+        }
+        cout << "Episode " << episode << " ended with score: " << totalReward << endl;
+        ale.reset_game();
+    }
+};
+
