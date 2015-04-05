b207874376d732337d24e677da60def8d09314ea
Marlos C. Machado <marlos.cholodovskis@gmail.com>
2015-04-04 19:12:14 -0600

    Fix: before, when compiling, SoundExporter was not being found.

--- src/common/module.mk
+++ src/common/module.mk
@@ -3,6 +3,7 @@ MODULE := src/common
 MODULE_OBJS := \
 	src/common/SoundNull.o \
 	src/common/SoundSDL.o \
+        src/common/SoundExporter.o\
 	src/common/display_screen.o \
 	src/common/ColourPalette.o \
 	src/common/ScreenExporter.o \
