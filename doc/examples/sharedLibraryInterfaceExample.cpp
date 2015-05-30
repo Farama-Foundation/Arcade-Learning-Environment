2812147aba7d1d06722ea1fcc6753de9400b66ad
Marc G. Bellemare <marcgb@gmail.com>
2015-05-30 19:01:40 +0100

    Fixed incorrect parameter name in example

--- doc/examples/sharedLibraryInterfaceExample.cpp
+++ doc/examples/sharedLibraryInterfaceExample.cpp
@@ -34,7 +34,7 @@ int main(int argc, char** argv) {
     // Get & Set the desired settings
     ale.setInt("random_seed", 123);
     //The default is already 0.25, this is just an example
-    ale.setFloat("stochasticity", 0.25);
+    ale.setFloat("repeat_action_probability", 0.25);
 
 #ifdef __USE_SDL
     ale.setBool("display_screen", true);
