afaa7d8ad7ab748ae48380c623405dc8b6752866
Jeff Juozapaitis <Jragonmiris@gmail.com>
2015-09-28 18:19:31 -0700

    Changed state encoding to not use raw malloc

--- ale_python_interface/ale_c_wrapper.cpp
+++ ale_python_interface/ale_c_wrapper.cpp
@@ -1,14 +1,11 @@
-//all code is currently in the .h file
 #include "ale_c_wrapper.h"
 #include<string>
 #include<cstring>
 
 const char *encodeState(ALEState *state) {
 	std::string s = state->encode();
-	int len = s.length();
-
-	char *buf = (char *)malloc(len+1);
-	strcpy(buf,s.c_str());
+	char *buf = new char[s.length() + 1];
+	std::strcpy(buf,s.c_str());
 
 	return buf;
 }
