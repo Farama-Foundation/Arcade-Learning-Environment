48b2639dd90174b4cc4f56a31d2635c92a4fcbaa
Sebastian Nowozin <nowozin@gmail.com>
2016-01-02 21:38:57 +0000

    Add.

--- src/games/Roms.cpp
+++ src/games/Roms.cpp
@@ -47,6 +47,7 @@
 #include "supported/JamesBond.hpp"
 #include "supported/JourneyEscape.hpp"
 #include "supported/Kangaroo.hpp"
+#include "supported/Kingkong.hpp"
 #include "supported/Krull.hpp"
 #include "supported/KungFuMaster.hpp"
 #include "supported/MontezumaRevenge.hpp"
@@ -113,6 +114,7 @@ static const RomSettings *roms[]  = {
     new JamesBondSettings(),
     new JourneyEscapeSettings(),
     new KangarooSettings(),
+    new KingkongSettings(),
     new KrullSettings(),
     new KungFuMasterSettings(),
     new MontezumaRevengeSettings(),
