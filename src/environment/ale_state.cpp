ba105d8ba4c5bda3d38ea4ac9ba254f9bee0eeca
Marlos C. Machado <marlos.cholodovskis@gmail.com>
2017-08-25 23:50:04 -0400

    Missing space.

--- src/environment/ale_state.cpp
+++ src/environment/ale_state.cpp
@@ -475,7 +475,7 @@ void ALEState::setActionJoysticks(Event* event, int player_a_action, int player_
  * ***************************************************************************/
 void ALEState::resetKeys(Event* event) {
     event->set(Event::ConsoleReset, 0);
-    event->set(Event::ConsoleSelect,0);
+    event->set(Event::ConsoleSelect, 0);
     event->set(Event::JoystickZeroFire, 0);
     event->set(Event::JoystickZeroUp, 0);
     event->set(Event::JoystickZeroDown, 0);
