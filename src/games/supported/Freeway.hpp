bcecf118ad6ec90ae672f0e6c095ff2a89d42136
Marlos C. Machado <marlos.cholodovskis@gmail.com>
2017-09-08 10:42:22 -0600

    Appropriate comment in Freeway.

--- src/games/supported/Freeway.hpp
+++ src/games/supported/Freeway.hpp
@@ -1,5 +1,5 @@
 /* *****************************************************************************
- * The line 67 is based on Xitari's code, from Google Inc.
+ * The method lives() is based on Xitari's code, from Google Inc.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2
