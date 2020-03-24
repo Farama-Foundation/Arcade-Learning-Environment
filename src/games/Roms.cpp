382373d01d21a904f8f35a2b0897ee167a160fd2
Thomas Köppe <tkoeppe@google.com>
2020-03-24 21:50:17 +0000

    [games] Several new games
    This change adds support code for the following games:
    
      * Atlantis2
      * Backgammon
      * BasicMath
      * Blackjack
      * Casino
      * Crossbow
      * DarkChambers
      * Earthworld
      * Entombed
      * ET
      * FlagCapture
      * Hangman
      * HauntedHouse
      * HumanCannonball
      * Klax
      * MarioBros
      * MiniatureGolf
      * Othello
      * Pacman
      * Pitfall2
      * SpaceWar
      * Superman
      * Surround
      * TicTacToe3D
      * VideoCheckers
      * VideoChess
      * VideoCube
      * WordZapper
    
    All available game difficulty settings and single-player game modes
    should be advertised.

--- src/games/Roms.cpp
+++ src/games/Roms.cpp
@@ -22,49 +22,69 @@
 #include "supported/Asterix.hpp"
 #include "supported/Asteroids.hpp"
 #include "supported/Atlantis.hpp"
+#include "supported/Atlantis2.hpp"
+#include "supported/Backgammon.hpp"
 #include "supported/BankHeist.hpp"
+#include "supported/BasicMath.hpp"
 #include "supported/BattleZone.hpp"
 #include "supported/BeamRider.hpp"
 #include "supported/Berzerk.hpp"
+#include "supported/Blackjack.hpp"
 #include "supported/Bowling.hpp"
 #include "supported/Boxing.hpp"
 #include "supported/Breakout.hpp"
 #include "supported/Carnival.hpp"
+#include "supported/Casino.hpp"
 #include "supported/Centipede.hpp"
 #include "supported/ChopperCommand.hpp"
 #include "supported/CrazyClimber.hpp"
+#include "supported/Crossbow.hpp"
+#include "supported/DarkChambers.hpp"
 #include "supported/Defender.hpp"
 #include "supported/DemonAttack.hpp"
 #include "supported/DonkeyKong.hpp"
 #include "supported/DoubleDunk.hpp"
+#include "supported/Earthworld.hpp"
 #include "supported/ElevatorAction.hpp"
 #include "supported/Enduro.hpp"
+#include "supported/Entombed.hpp"
+#include "supported/Et.hpp"
 #include "supported/FishingDerby.hpp"
+#include "supported/FlagCapture.hpp"
 #include "supported/Freeway.hpp"
 #include "supported/Frogger.hpp"
 #include "supported/Frostbite.hpp"
 #include "supported/Galaxian.hpp"
 #include "supported/Gopher.hpp"
 #include "supported/Gravitar.hpp"
+#include "supported/Hangman.hpp"
+#include "supported/HauntedHouse.hpp"
 #include "supported/Hero.hpp"
+#include "supported/HumanCannonball.hpp"
 #include "supported/IceHockey.hpp"
 #include "supported/JamesBond.hpp"
 #include "supported/JourneyEscape.hpp"
 #include "supported/Kaboom.hpp"
 #include "supported/Kangaroo.hpp"
-#include "supported/Koolaid.hpp"
 #include "supported/KeystoneKapers.hpp"
 #include "supported/Kingkong.hpp"
+#include "supported/Klax.hpp"
+#include "supported/Koolaid.hpp"
 #include "supported/Krull.hpp"
 #include "supported/KungFuMaster.hpp"
 #include "supported/LaserGates.hpp"
 #include "supported/LostLuggage.hpp"
+#include "supported/MarioBros.hpp"
+#include "supported/MiniatureGolf.hpp"
 #include "supported/MontezumaRevenge.hpp"
 #include "supported/MrDo.hpp"
 #include "supported/MsPacman.hpp"
 #include "supported/NameThisGame.hpp"
+#include "supported/Othello.hpp"
+#include "supported/Pacman.hpp"
 #include "supported/Phoenix.hpp"
 #include "supported/Pitfall.hpp"
+#include "supported/Pitfall2.hpp"
 #include "supported/Pong.hpp"
 #include "supported/Pooyan.hpp"
 #include "supported/PrivateEye.hpp"
@@ -77,17 +97,25 @@
 #include "supported/Skiing.hpp"
 #include "supported/Solaris.hpp"
 #include "supported/SpaceInvaders.hpp"
+#include "supported/SpaceWar.hpp"
 #include "supported/StarGunner.hpp"
+#include "supported/Superman.hpp"
+#include "supported/Surround.hpp"
 #include "supported/Tennis.hpp"
 #include "supported/Tetris.hpp"
+#include "supported/TicTacToe3d.hpp"
 #include "supported/TimePilot.hpp"
-#include "supported/Turmoil.hpp"
 #include "supported/Trondead.hpp"
+#include "supported/Turmoil.hpp"
 #include "supported/Tutankham.hpp"
 #include "supported/UpNDown.hpp"
 #include "supported/Venture.hpp"
+#include "supported/VideoCheckers.hpp"
+#include "supported/VideoChess.hpp"
+#include "supported/VideoCube.hpp"
 #include "supported/VideoPinball.hpp"
 #include "supported/WizardOfWor.hpp"
+#include "supported/WordZapper.hpp"
 #include "supported/YarsRevenge.hpp"
 #include "supported/Zaxxon.hpp"
 
@@ -103,31 +131,45 @@ static const RomSettings* roms[] = {
     new AsterixSettings(),
     new AsteroidsSettings(),
     new AtlantisSettings(),
+    new Atlantis2Settings(),
+    new BackgammonSettings(),
     new BankHeistSettings(),
+    new BasicMathSettings(),
     new BattleZoneSettings(),
     new BeamRiderSettings(),
     new BerzerkSettings(),
+    new BlackjackSettings(),
     new BowlingSettings(),
     new BoxingSettings(),
     new BreakoutSettings(),
     new CarnivalSettings(),
+    new CasinoSettings(),
     new CentipedeSettings(),
     new ChopperCommandSettings(),
     new CrazyClimberSettings(),
+    new CrossbowSettings(),
+    new DarkChambersSettings(),
     new DefenderSettings(),
     new DemonAttackSettings(),
     new DonkeyKongSettings(),
     new DoubleDunkSettings(),
+    new EarthworldSettings(),
     new ElevatorActionSettings(),
     new EnduroSettings(),
+    new EntombedSettings(),
+    new EtSettings(),
     new FishingDerbySettings(),
+    new FlagCaptureSettings(),
     new FreewaySettings(),
     new FroggerSettings(),
     new FrostbiteSettings(),
     new GalaxianSettings(),
     new GopherSettings(),
     new GravitarSettings(),
+    new HangmanSettings(),
+    new HauntedHouseSettings(),
     new HeroSettings(),
+    new HumanCannonballSettings(),
     new IceHockeySettings(),
     new JamesBondSettings(),
     new JourneyEscapeSettings(),
@@ -136,16 +178,22 @@ static const RomSettings* roms[] = {
     new KoolaidSettings(),
     new KeystoneKapersSettings(),
     new KingkongSettings(),
+    new KlaxSettings(),
     new KrullSettings(),
     new KungFuMasterSettings(),
     new LaserGatesSettings(),
     new LostLuggageSettings(),
+    new MarioBrosSettings(),
+    new MiniatureGolfSettings(),
     new MontezumaRevengeSettings(),
     new MrDoSettings(),
     new MsPacmanSettings(),
     new NameThisGameSettings(),
+    new OthelloSettings(),
+    new PacmanSettings(),
     new PhoenixSettings(),
     new PitfallSettings(),
+    new Pitfall2Settings(),
     new PongSettings(),
     new PooyanSettings(),
     new PrivateEyeSettings(),
@@ -158,17 +206,25 @@ static const RomSettings* roms[] = {
     new SkiingSettings(),
     new SolarisSettings(),
     new SpaceInvadersSettings(),
+    new SpaceWarSettings(),
     new StarGunnerSettings(),
+    new SupermanSettings(),
+    new SurroundSettings(),
     new TennisSettings(),
     new TetrisSettings(),
+    new TicTacToe3dSettings(),
     new TimePilotSettings(),
     new TurmoilSettings(),
     new TrondeadSettings(),
     new TutankhamSettings(),
     new UpNDownSettings(),
     new VentureSettings(),
+    new VideoCheckersSettings(),
+    new VideoChessSettings(),
+    new VideoCubeSettings(),
     new VideoPinballSettings(),
     new WizardOfWorSettings(),
+    new WordZapperSettings(),
     new YarsRevengeSettings(),
     new ZaxxonSettings(),
 };
---
+++ src/games/supported/Hangman.hpp
@@ -0,0 +1,85 @@
+/* *****************************************************************************
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * *****************************************************************************
+ * A.L.E (Arcade Learning Environment)
+ * Copyright (c) 2009-2013 by Yavar Naddaf, Joel Veness, Marc G. Bellemare and
+ *   the Reinforcement Learning and Artificial Intelligence Laboratory
+ * Released under the GNU General Public License; see License.txt for details.
+ *
+ * Based on: Stella  --  "An Atari 2600 VCS Emulator"
+ * Copyright (c) 1995-2007 by Bradford W. Mott and the Stella team
+ *
+ * *****************************************************************************
+ */
+
+#ifndef __HANGMAN_HPP__
+#define __HANGMAN_HPP__
+
+#include "../RomSettings.hpp"
+
+namespace ale {
+
+// RL wrapper for Hangman
+class HangmanSettings : public RomSettings {
+ public:
+  HangmanSettings();
+
+  // reset
+  void reset() override;
+
+  // is end of game
+  bool isTerminal() const override;
+
+  // get the most recently observed reward
+  reward_t getReward() const override;
+
+  // the rom-name
+  const char* rom() const override { return "hangman"; }
+
+
+  // create a new instance of the rom
+  RomSettings* clone() const override;
+
+  // is an action part of the minimal set?
+  bool isMinimal(const Action& a) const override;
+
+  // process the latest information from ALE
+  void step(const System& system) override;
+
+  // saves the state of the rom settings
+  void saveState(Serializer& ser) override;
+
+  // loads the state of the rom settings
+  void loadState(Deserializer& ser) override;
+
+  ModeVect getAvailableModes() override;
+
+  void setMode(game_mode_t m, System& system,
+               std::unique_ptr<StellaEnvironmentWrapper> environment) override;
+
+  // returns a list of difficulties that the game can be played in
+  // in this game, there are 2 available difficulties
+  DifficultyVect getAvailableDifficulties() override;
+
+ private:
+  bool m_terminal;
+  int m_timerArray[2];
+  reward_t m_reward;
+  reward_t m_computerScore;
+  reward_t m_playerScore;
+};
+
+}  // namespace ale
+
+#endif  // __HANGMAN_HPP__
