c20f33e338e825ec4c7d0ede446293851b6e787b
Jesse Farebrother <jessefarebro@gmail.com>
2020-03-31 08:46:04 -0600

    Remove deprecated examples, update remaining examples
    With the removal of controllers many of the examples are no longer
    relevant. The remaining Python and C++ examples have been updated
    accordingly.

---
+++ examples/python-interface/python_example.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# python_example.py
+# Author: Ben Goodrich
+#
+# This is a direct port to python of the shared library example from
+# ALE provided in examples/sharedLibraryInterfaceExample.cpp
+import sys
+from random import randrange
+from ale_py import ALEInterface
+
+if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} rom_file")
+    sys.exit()
+
+ale = ALEInterface()
+
+# Get & Set the desired settings
+ale.setInt("random_seed", 123)
+
+# Set USE_SDL to true to display the screen. ALE must be compilied
+# with SDL enabled for this to work. On OSX, pygame init is used to
+# proxy-call SDL_main.
+USE_SDL = False
+if USE_SDL:
+    ale.setBool("sound", True)
+    ale.setBool("display_screen", True)
+
+# Load the ROM file
+rom_file = sys.argv[1]
+ale.loadROM(rom_file)
+
+# Get the list of legal actions
+legal_actions = ale.getLegalActionSet()
+
+# Play 10 episodes
+for episode in range(10):
+    total_reward = 0
+    while not ale.game_over():
+        a = legal_actions[randrange(len(legal_actions))]
+        # Apply an action and get the resulting reward
+        reward = ale.act(a)
+        total_reward += reward
+    print("Episode %d ended with score: %d" % (episode, total_reward))
+    ale.reset_game()
