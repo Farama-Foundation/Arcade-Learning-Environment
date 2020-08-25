8f371e4ea9884d8b8b803f6b32f83990122719d9
Thomas Köppe <tkoeppe@google.com>
2020-08-25 13:10:26 +0100

    [ale_interface] Separate "getString" into copying and non-copying versions.
    The non-copying version is useful for Python bindings in other
    projects, since it allows viewing the emulator's internal storage
    without requiring the caller to manage the storage.

--- src/ale_interface.cpp
+++ src/ale_interface.cpp
@@ -218,9 +218,12 @@ void ALEInterface::loadROM(std::string rom_file) {
 }
 
 // Get the value of a setting.
-std::string ALEInterface::getString(const std::string& key) {
+const std::string& ALEInterface::getStringInplace(const std::string& key) {
   assert(theSettings.get());
   return theSettings->getString(key);
+}
+std::string ALEInterface::getString(const std::string& key) {
+  return getStringInplace(key);
 }
 int ALEInterface::getInt(const std::string& key) {
   assert(theSettings.get());
