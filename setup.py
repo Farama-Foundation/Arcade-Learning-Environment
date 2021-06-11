25544ab1bbb9396bde078ff547211328c419a0ad
Jesse Farebrother <jessefarebro@gmail.com>
2021-06-11 14:26:35 -0600

    Python build updates, preparation for conda builds

--- setup.py
+++ setup.py
@@ -40,12 +40,17 @@ class CMakeBuild(build_ext):
             extdir += os.path.sep
 
         config = "Debug" if self.debug else "Release"
+
+        # CMake lets you override the generator - we need to check this.
+        # Can be set with Conda-Build, for example.
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+
         cmake_args = [
             f"-DCMAKE_BUILD_TYPE={config}",
-            "-DBUILD_CPP_LIB=OFF",
-            "-DBUILD_PYTHON_LIB=ON",
-            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{config.upper()}={extdir}",
             f"-DPython3_EXECUTABLE={sys.executable}",
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
+            "-DBUILD_CPP_LIB=OFF",
+            "-DBUILD_PYTHON_LIB=ON"
         ]
         build_args = []
 
@@ -55,10 +60,27 @@ class CMakeBuild(build_ext):
             # exported for Ninja to pick it up, which is a little tricky to do.
             # Users can override the generator with CMAKE_GENERATOR in CMake
             # 3.15+.
-            cmake_args += ["-GNinja"]
+            if not cmake_generator:
+                cmake_args += ["-GNinja"]
         else:
-            cmake_args += ["-A", self.PLAT_TO_CMAKE[self.plat_name]]
-            build_args += ["--config", config]
+            # Single config generators are handled "normally"
+            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
+
+            # CMake allows an arch-in-generator style for backward compatibility
+            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
+
+            # Specify the arch if using MSVC generator, but only if it doesn't
+            # contain a backward-compatibility arch spec already in the
+            # generator name.
+            if not single_config and not contains_arch:
+                cmake_args += ["-A", self.PLAT_TO_CMAKE[self.plat_name]]
+
+            # Multi-config generators have a different way to specify configs
+            if not single_config:
+                cmake_args += [
+                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{config.upper()}={extdir}"
+                ]
+                build_args += ["--config", config]
 
         if self.distribution.cmake_options is not None:
             cmake_args += shlex.split(self.distribution.cmake_options)
@@ -74,11 +96,12 @@ class CMakeBuild(build_ext):
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
-        os.chdir(self.build_temp)
-        self.spawn(["cmake", ext.sourcedir] + cmake_args)
-        if not self.dry_run:
-            self.spawn(["cmake", "--build", "."] + build_args)
-        os.chdir(here)
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
+        )
+        subprocess.check_call(
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
+        )
 
 
 def parse_version(version_file):
