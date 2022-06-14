fdbd6a859711c3800e6c77908486c55405f0eede
Jesse Farebrother <jessefarebro@gmail.com>
2022-06-14 14:42:23 -0400

    Refactor import-roms

--- src/python/scripts/import_roms.py
+++ src/python/scripts/import_roms.py
@@ -1,47 +1,38 @@
-import os
 import argparse
 import pathlib
-import operator
+import shutil
 import warnings
+from typing import Optional
 
+import ale_py
 import importlib_resources as resources
 
-from shutil import copyfile
 
-from ale_py import __version__, ALEInterface
-
-
-def scantree(dir, recurse=True):
-    """
-    Recursive os.scandir
-    """
-    with os.scandir(dir) as root:
-        for entry in root:
-            if recurse and entry.is_dir(follow_symlinks=False):
-                yield from scantree(entry.path)
-            elif entry.is_file():
-                yield pathlib.Path(entry)
-
-
-def import_roms(romdir, datadir, pkg=None, dry_run=False):
+def import_roms(
+    romdir: pathlib.Path,
+    datadir: pathlib.Path,
+    pkg: Optional[str] = None,
+    dry_run: bool = False,
+) -> None:
     """
     Recursively copies all compatible ROMs in romdir
     to datadir using the proper filename for the ALE.
     """
-    # Get all files in romdir ending in '.bin'
-    entries = list(filter(lambda file: file.suffix == ".bin", scantree(romdir)))
-    # Get ROM id or None from isSupportedROM
-    ids = list(map(ALEInterface.isSupportedROM, entries))
-    # Filter only the supported ROMs
-    supported = list(filter(lambda args: args[0] is not None, zip(ids, entries)))
-    # Get the set of ROMs that are unsupported
-    unsupported = set(entries) - set(map(operator.itemgetter(1), supported))
+
+    supported = {}
+    unsupported = []
+    for path in romdir.glob("**/*.bin"):
+        rom = ale_py.ALEInterface.isSupportedROM(path)
+        if rom is not None:
+            supported[rom] = path
+        else:
+            unsupported.append(path)
 
     # Copy over supported files
-    for rom, path in supported:
+    for rom, path in supported.items():
         identifier = str(path) if pkg is None else f"{pkg}/{path.name}"
         if not dry_run:
-            copyfile(path, datadir / f"{rom}.bin")
+            shutil.copyfile(path, datadir / f"{rom}.bin")
         print(f"\033[92m{'[SUPPORTED]': <15}\033[0m {rom: >20} {identifier: >30}")
 
     print("\n")
@@ -52,15 +43,15 @@ def import_roms(romdir, datadir, pkg=None, dry_run=False):
 
     # Print summary
     if not dry_run:
-        print(f"\nImported {len(supported)} / {len(entries)} ROMs")
+        print(f"\nImported {len(supported)} / {len(supported)} ROMs")
 
 
-def main():
+def main() -> None:
     """
     CLI for ale-import-roms
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--version", action="version", version=__version__)
+    parser.add_argument("--version", action="version", version=ale_py.__version__)
     parser.add_argument("--dry-run", action="store_true")
 
     group = parser.add_mutually_exclusive_group(required=True)
@@ -76,20 +67,22 @@ def main():
             print(f"Path {romdir} doesn't exist.")
             exit(1)
     elif args.import_from_pkg:
-        root, subpackage = args.import_from_pkg.split(".", maxsplit=1)
+        if "." in args.import_from_pkg:
+            root, subpackage = args.import_from_pkg.split(".", maxsplit=1)
+        else:
+            root, subpackage = args.import_from_pkg, None
         try:
             with resources.path(root, subpackage) as path:
                 romdir = path.resolve()
+                if not romdir.exists():
+                    print(f"Unable to find path {subpackage} in module {root}.")
+                    exit(1)
         except ModuleNotFoundError:
             print(f"Unable to find module {root}.")
             exit(1)
         except Exception as e:
             print(f"Unknown error {str(e)}.")
             exit(1)
-        finally:
-            if not romdir.exists():
-                print(f"Unable to find path {subpackage} in module {root}.")
-                exit(1)
 
     with warnings.catch_warnings():
         warnings.filterwarnings(
