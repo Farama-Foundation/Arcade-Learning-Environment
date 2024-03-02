9f7c39cdb0739bfdb3d3889f859cdfe71275d812
Mark Towers <mark.m.towers@gmail.com>
2024-03-02 17:32:43 +0000

    Update the check_env test for all environments (#514)

--- tests/python/test_atari_env.py
+++ tests/python/test_atari_env.py
@@ -15,12 +15,29 @@ from utils import test_rom_path, tetris_env  # noqa: F401
     "env_id",
     [
         env_id
-        for env_id in gymnasium.registry.keys()
-        if (env_id.startswith("ALE/") and (env_id.endswith("-v5")))
+        for env_id, spec in gymnasium.registry.items()
+        if spec.entry_point == "ale_py.env:AtariEnv"
     ],
 )
 def test_check_env(env_id):
-    check_env(gymnasium.make(env_id))
+    if any(
+        unsupported_game in env_id
+        for unsupported_game in ["Warlords", "MazeCraze", "Joust", "Combat"]
+    ):
+        pytest.skip(env_id)
+
+    with warnings.catch_warnings(record=True) as caught_warnings:
+        env = gymnasium.make(env_id).unwrapped
+        check_env(env, skip_render_check=True)
+
+        env.close()
+
+    for warning in caught_warnings:
+        if (
+            "is out of date. You should consider upgrading to version"
+            not in warning.message.args[0]
+        ):
+            raise ValueError(warning.message.args[0])
 
 
 def test_register_legacy_env_id():
