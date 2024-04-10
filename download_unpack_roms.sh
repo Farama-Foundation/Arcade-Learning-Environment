f556b96bf88fda9eb1bce71d6d618f42fd7b6de5
Jet <taijunjet@hotmail.com>
2024-04-10 14:04:17 +0100

    better names

--- download_unpack_roms.sh
+++ download_unpack_roms.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # define some directories
-this_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-unpack_dir="${this_directory}/unpack_dir"
-target_dir="${this_directory}/src/python/roms"
+this_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+unpack_dir="${this_dir}/unpack_dir"
+target_dir="${this_dir}/src/python/roms"
 
 # make the directory where we will do the unpacking
 mkdir $unpack_dir
 
+# download the ROMs from the git gist, decode it, then unpack it
 curl "https://gist.githubusercontent.com/jjshoots/61b22aefce4456920ba99f2c36906eda/raw/00046ac3403768bfe45857610a3d333b8e35e026/Roms.tar.gz.b64" \
   | base64 --decode \
   | tar -xzf - -C $unpack_dir
