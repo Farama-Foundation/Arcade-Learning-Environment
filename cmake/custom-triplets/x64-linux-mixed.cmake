9ac218a70f5d46a69504e7196426a02c63abf392
Mark Towers <mark.m.towers@gmail.com>
2024-04-24 00:19:54 +0100

    Pull Gym env in favor of Gymnasium and include ROMs with PyPI (#516)
    Co-authored-by: Jet <38184875+jjshoots@users.noreply.github.com>

--- cmake/custom-triplets/x64-linux-mixed.cmake
+++ cmake/custom-triplets/x64-linux-mixed.cmake
@@ -1,4 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
 if(PORT MATCHES "sdl")
   set(VCPKG_LIBRARY_LINKAGE dynamic)
   set(VCPKG_CRT_LINKAGE dynamic)
@@ -6,5 +9,3 @@ else()
   set(VCPKG_LIBRARY_LINKAGE static)
   set(VCPKG_CRT_LINKAGE static)
 endif()
-
-set(VCPKG_CMAKE_SYSTEM_NAME Linux)
