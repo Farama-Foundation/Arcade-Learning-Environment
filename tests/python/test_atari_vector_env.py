72a3f20d6d2809d969f3e34446b012ad34950635
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2025-05-24 14:01:56 +0100

    Add autoreset mode tests

--- tests/python/test_atari_vector_env.py
+++ tests/python/test_atari_vector_env.py
@@ -62,12 +62,83 @@ def test_reset_step_shapes(num_envs, stack_num, img_height, img_width, grayscale
     envs.close()
 
 
+@pytest.mark.parametrize(
+    "num_envs, seeds",
+    [(1, np.array([0])), (3, np.array([1, 2, 3])), (10, np.arange(10))],
+)
+@pytest.mark.parametrize("noop_max", (0, 10, 30))
+@pytest.mark.parametrize("repeat_action_probability", (0.0, 0.25))
+@pytest.mark.parametrize("use_fire_reset", [False, True])
+def test_determinism(
+    num_envs: int,
+    seeds: np.ndarray,
+    noop_max: int,
+    repeat_action_probability: float,
+    use_fire_reset: bool,
+    game: str = "pong",
+    rollout_length: int = 100,
+):
+    envs_1 = AtariVectorEnv(
+        game,
+        num_envs=num_envs,
+        noop_max=noop_max,
+        repeat_action_probability=repeat_action_probability,
+        use_fire_reset=use_fire_reset,
+    )
+    envs_2 = AtariVectorEnv(
+        game,
+        num_envs=num_envs,
+        noop_max=noop_max,
+        repeat_action_probability=repeat_action_probability,
+        use_fire_reset=use_fire_reset,
+    )
+
+    obs_1, info_1 = envs_1.reset(seed=seeds)
+    obs_2, info_2 = envs_2.reset(seed=seeds)
+
+    assert data_equivalence(obs_1, obs_2)
+    assert data_equivalence(info_1, info_2)
+
+    for i in range(rollout_length):
+        actions = envs_1.action_space.sample()
+
+        obs_1, rewards_1, terminations_1, truncations_1, info_1 = envs_1.step(actions)
+        obs_2, rewards_2, terminations_2, truncations_2, info_2 = envs_2.step(actions)
+
+        assert data_equivalence(obs_1, obs_2)
+        assert data_equivalence(rewards_1, rewards_2)
+        assert data_equivalence(terminations_1, terminations_2)
+        assert data_equivalence(truncations_1, truncations_2)
+        assert data_equivalence(info_1, info_2)
+
+    envs_1.close()
+    envs_2.close()
+
+
+def obs_equivalence(gym_obs, ale_obs, i, **log_kwargs):
+    # There is a difference between the MacOS ARM and Python implement with minimal differences
+    diff = gym_obs.astype(np.int32) - ale_obs.astype(np.int32)
+    count = np.count_nonzero(diff)
+
+    assert gym_obs.shape == ale_obs.shape, i
+    assert gym_obs.dtype == ale_obs.dtype, i
+    assert np.max(diff) <= 2, i
+    assert np.min(diff) >= -2, i
+    assert count <= 5, i
+
+    gym.logger.warn(
+        f"rollout obs diff for timestep={i}, max diff={np.max(diff)}, min diff={np.min(diff)}, non-zero count={count}, params={log_kwargs}"
+    )
+    return True
+
+
 def assert_rollout_equivalence(
     gym_envs,
     ale_envs,
     rollout_length=100,
     reset_seed=123,
     action_seed=123,
+    **log_kwargs,
 ):
     """Test if both environments produce similar results over a short rollout."""
     assert gym_envs.num_envs == ale_envs.num_envs
@@ -99,21 +170,7 @@ def assert_rollout_equivalence(
             ale_envs.step(actions)
         )
 
-        if not data_equivalence(gym_obs, ale_obs):
-            # For MacOS ARM, there is a known problem where there is a max difference of 1 for 1 or 2 pixels
-            diff = gym_obs.astype(np.int32) - ale_obs.astype(np.int32)
-            count = np.count_nonzero(diff)
-
-            assert gym_obs.shape == ale_obs.shape
-            assert gym_obs.dtype == ale_obs.dtype
-            assert np.max(diff) <= 2
-            assert np.min(diff) >= -2
-            assert count <= 5
-
-            gym.logger.warn(
-                f"rollout obs diff for timestep={i}, max diff={np.max(diff)}, min diff={np.min(diff)}, non-zero count={count}"
-            )
-
+        assert obs_equivalence(gym_obs, ale_obs, i, **log_kwargs)
         assert data_equivalence(gym_rewards.astype(np.int32), ale_rewards), i
         assert data_equivalence(gym_terminations, ale_terminations), i
         assert data_equivalence(gym_truncations, ale_truncations), i
@@ -167,7 +224,15 @@ def test_obs_params_equivalence(
         grayscale=grayscale,
     )
 
-    assert_rollout_equivalence(gym_envs, ale_envs)
+    assert_rollout_equivalence(
+        gym_envs,
+        ale_envs,
+        stack_num=stack_num,
+        img_height=img_height,
+        img_width=img_width,
+        frame_skip=frame_skip,
+        grayscale=grayscale,
+    )
 
 
 @pytest.mark.parametrize("continuous_action_threshold", (0.2, 0.5, 0.8))
@@ -198,60 +263,194 @@ def test_continuous_equivalence(continuous_action_threshold, num_envs=8):
         continuous_action_threshold=continuous_action_threshold,
     )
 
-    assert_rollout_equivalence(gym_envs, ale_envs)
+    assert_rollout_equivalence(
+        gym_envs, ale_envs, continuous_action_threshold=continuous_action_threshold
+    )
 
 
-@pytest.mark.parametrize(
-    "num_envs, seeds",
-    [(1, np.array([0])), (3, np.array([1, 2, 3])), (10, np.arange(10))],
-)
-@pytest.mark.parametrize("noop_max", (0, 10, 30))
-@pytest.mark.parametrize("repeat_action_probability", (0.0, 0.25))
-@pytest.mark.parametrize("use_fire_reset", [False, True])
-def test_determinism(
-    num_envs: int,
-    seeds: np.ndarray,
-    noop_max: int,
-    repeat_action_probability: float,
-    use_fire_reset: bool,
-    game: str = "pong",
-    rollout_length: int = 100,
+def test_disable_autoreset_mode(
+    num_envs=4, reset_seed=123, action_seed=123, rollout_length=100
 ):
-    envs_1 = AtariVectorEnv(
-        game,
-        num_envs=num_envs,
-        noop_max=noop_max,
-        repeat_action_probability=repeat_action_probability,
-        use_fire_reset=use_fire_reset,
+    """Test if both environments produce similar results over a short rollout."""
+    gym_envs = gym.vector.SyncVectorEnv(
+        [
+            lambda: gym.wrappers.FrameStackObservation(
+                gym.wrappers.AtariPreprocessing(
+                    gym.make(
+                        "BreakoutNoFrameskip-v4",
+                    ),
+                    noop_max=0,
+                ),
+                stack_size=4,
+                padding_type="zero",
+            )
+            for _ in range(num_envs)
+        ],
+        autoreset_mode=gym.vector.AutoresetMode.DISABLED,
     )
-    envs_2 = AtariVectorEnv(
-        game,
+    ale_envs = AtariVectorEnv(
+        game="breakout",
         num_envs=num_envs,
-        noop_max=noop_max,
-        repeat_action_probability=repeat_action_probability,
-        use_fire_reset=use_fire_reset,
+        noop_max=0,
+        use_fire_reset=False,
+        autoreset_mode=gym.vector.AutoresetMode.DISABLED,
     )
+    assert gym_envs.metadata["autoreset_mode"] == ale_envs.metadata["autoreset_mode"]
 
-    obs_1, info_1 = envs_1.reset(seed=seeds)
-    obs_2, info_2 = envs_2.reset(seed=seeds)
+    gym_obs, gym_info = gym_envs.reset(seed=reset_seed)
+    ale_obs, ale_info = ale_envs.reset(seed=reset_seed)
 
-    assert data_equivalence(obs_1, obs_2)
-    assert data_equivalence(info_1, info_2)
+    assert data_equivalence(gym_obs, ale_obs)
 
+    gym_info = {
+        key: value.astype(np.int32)
+        for key, value in gym_info.items()
+        if not key.startswith("_") and key != "seeds"
+    }
+    env_ids = ale_info.pop("env_id")
+    assert np.all(env_ids == np.arange(gym_envs.num_envs))
+    assert data_equivalence(gym_info, ale_info)
+
+    ale_envs.action_space.seed(action_seed)
     for i in range(rollout_length):
-        actions = envs_1.action_space.sample()
+        actions = ale_envs.action_space.sample()
 
-        obs_1, rewards_1, terminations_1, truncations_1, info_1 = envs_1.step(actions)
-        obs_2, rewards_2, terminations_2, truncations_2, info_2 = envs_2.step(actions)
+        gym_obs, gym_rewards, gym_terminations, gym_truncations, gym_info = (
+            gym_envs.step(actions)
+        )
+        ale_obs, ale_rewards, ale_terminations, ale_truncations, ale_info = (
+            ale_envs.step(actions)
+        )
 
-        assert data_equivalence(obs_1, obs_2)
-        assert data_equivalence(rewards_1, rewards_2)
-        assert data_equivalence(terminations_1, terminations_2)
-        assert data_equivalence(truncations_1, truncations_2)
-        assert data_equivalence(info_1, info_2)
+        assert obs_equivalence(gym_obs, ale_obs, i, autoreset_mode="DISABLED"), i
+        assert data_equivalence(gym_rewards.astype(np.int32), ale_rewards), i
+        assert data_equivalence(gym_terminations, ale_terminations), i
+        assert data_equivalence(gym_truncations, ale_truncations), i
 
-    envs_1.close()
-    envs_2.close()
+        gym_info = {
+            key: value.astype(np.int32)
+            for key, value in gym_info.items()
+            if not key.startswith("_") and key != "seeds"
+        }
+        env_ids = ale_info.pop("env_id")
+        assert np.all(env_ids == np.arange(gym_envs.num_envs)), i
+        assert data_equivalence(gym_info, ale_info), i
+
+        reset_mask, _ = np.where(np.logical_or(gym_terminations, gym_truncations))
+        if np.any(reset_mask):
+            gym_obs, gym_info = gym_envs.reset(options={"reset_mask": reset_mask})
+            ale_obs, ale_info = ale_envs.reset(options={"reset_mask": reset_mask})
+
+            assert obs_equivalence(gym_obs, ale_obs, i, autoreset_mode="DISABLED"), i
+            gym_info = {
+                key: value.astype(np.int32)
+                for key, value in gym_info.items()
+                if not key.startswith("_") and key != "seeds"
+            }
+            env_ids = ale_info.pop("env_id")
+            assert np.all(env_ids == np.arange(gym_envs.num_envs)), i
+            assert data_equivalence(gym_info, ale_info), i
+
+    gym_envs.close()
+    ale_envs.close()
+
+
+def test_same_step_autoreset_mode(
+    num_envs=4, reset_seed=123, action_seed=123, rollout_length=100
+):
+    """Test if both environments produce similar results over a short rollout."""
+    gym_envs = gym.vector.SyncVectorEnv(
+        [
+            lambda: gym.wrappers.FrameStackObservation(
+                gym.wrappers.AtariPreprocessing(
+                    gym.make(
+                        "BreakoutNoFrameskip-v4",
+                    ),
+                    noop_max=0,
+                ),
+                stack_size=4,
+                padding_type="zero",
+            )
+            for _ in range(num_envs)
+        ],
+        autoreset_mode=gym.vector.AutoresetMode.SAME_STEP,
+    )
+    ale_envs = AtariVectorEnv(
+        game="breakout",
+        num_envs=num_envs,
+        noop_max=0,
+        use_fire_reset=False,
+        autoreset_mode=gym.vector.AutoresetMode.SAME_STEP,
+    )
+    assert gym_envs.metadata["autoreset_mode"] == ale_envs.metadata["autoreset_mode"]
+
+    gym_obs, gym_info = gym_envs.reset(seed=reset_seed)
+    ale_obs, ale_info = ale_envs.reset(seed=reset_seed)
+
+    assert data_equivalence(gym_obs, ale_obs)
+
+    gym_info = {
+        key: value.astype(np.int32)
+        for key, value in gym_info.items()
+        if not key.startswith("_") and key != "seeds"
+    }
+    env_ids = ale_info.pop("env_id")
+    assert np.all(env_ids == np.arange(gym_envs.num_envs))
+    assert data_equivalence(gym_info, ale_info)
+
+    ale_envs.action_space.seed(action_seed)
+    for i in range(rollout_length):
+        actions = ale_envs.action_space.sample()
+
+        gym_obs, gym_rewards, gym_terminations, gym_truncations, gym_info = (
+            gym_envs.step(actions)
+        )
+        ale_obs, ale_rewards, ale_terminations, ale_truncations, ale_info = (
+            ale_envs.step(actions)
+        )
+
+        assert obs_equivalence(gym_obs, ale_obs, i, autoreset_mode="SAME-STEP"), i
+        assert data_equivalence(gym_rewards.astype(np.int32), ale_rewards), i
+        assert data_equivalence(gym_terminations, ale_terminations), i
+        assert data_equivalence(gym_truncations, ale_truncations), i
+
+        gym_info = {
+            key: value.astype(np.int32)
+            for key, value in gym_info.items()
+            if not key.startswith("_") and key != "seeds"
+        }
+        env_ids = ale_info.pop("env_id")
+        assert np.all(env_ids == np.arange(gym_envs.num_envs)), i
+
+        reset_mask, _ = np.where(np.logical_or(gym_terminations, gym_truncations))
+        if np.any(reset_mask):
+            gym_final_obs = gym_info.pop("final_observation")
+            gym_info.pop("final_env_info")  # ALEV doesn't return final info
+
+            ale_final_obs = ale_info.pop("final_observation")
+            assert data_equivalence(gym_info, ale_info), i
+            assert obs_equivalence(
+                gym_final_obs, ale_final_obs, i, autoreset_mode="SAME-STEP"
+            ), i
+
+            # disable reset
+            gym_obs, gym_info = gym_envs.reset(options={"reset_mask": reset_mask})
+            ale_obs, ale_info = ale_envs.reset(options={"reset_mask": reset_mask})
+
+            assert obs_equivalence(gym_obs, ale_obs, i, autoreset_mode="SAME-STEP"), i
+            gym_info = {
+                key: value.astype(np.int32)
+                for key, value in gym_info.items()
+                if not key.startswith("_") and key != "seeds"
+            }
+            env_ids = ale_info.pop("env_id")
+            assert np.all(env_ids == np.arange(gym_envs.num_envs)), i
+            assert data_equivalence(gym_info, ale_info), i
+        else:
+            assert data_equivalence(gym_info, ale_info), i
+
+    gym_envs.close()
+    ale_envs.close()
 
 
 def test_batch_size_async():
