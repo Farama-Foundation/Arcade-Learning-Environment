5fd62737c3d5d5383a279e23ec617d1996ad82a5
Mark Towers <mark.m.towers@gmail.com>
2025-05-26 11:30:30 +0100

    Add tests on vector `episodic_life` and `life_loss_info` (#611)

--- src/ale/vector/preprocessed_env.hpp
+++ src/ale/vector/preprocessed_env.hpp
@@ -167,7 +167,7 @@ namespace ale::vector {
             std::fill(raw_frames_[1].begin(), raw_frames_[1].end(), 0);
 
             // Clear the frame stack
-            for (int stack_id = 0; stack_id < stack_num_ - 1; ++stack_id) {
+            for (int stack_id = 0; stack_id < stack_num_; ++stack_id) {
                 std::fill(frame_stack_[stack_id].begin(), frame_stack_[stack_id].end(), 0);
             }
             process_screen();
@@ -239,7 +239,7 @@ namespace ale::vector {
             timestep.env_id = env_id_;
 
             timestep.reward = reward_;
-            timestep.terminated = game_over_ || (life_loss_info_ && was_life_loss_);
+            timestep.terminated = game_over_ || ((life_loss_info_ || episodic_life_) && was_life_loss_);
             timestep.truncated = elapsed_step_ >= max_episode_steps_;
 
             timestep.lives = lives_;
@@ -265,7 +265,7 @@ namespace ale::vector {
          * Check if the episode is over (terminated or truncated)
          */
         bool is_episode_over() const {
-            return game_over_ || elapsed_step_ >= max_episode_steps_;
+            return game_over_ || elapsed_step_ >= max_episode_steps_ || (episodic_life_ && was_life_loss_);
         }
 
         /**
