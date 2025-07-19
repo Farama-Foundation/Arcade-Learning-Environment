18862fa7edb0b4e0f126efa0b7b5711f1b4da135
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2025-07-19 22:31:46 +0100

    Add windows-mixed triplet

--- cmake/custom-triplets/arm64-linux-mixed.cmake
+++ cmake/custom-triplets/arm64-linux-mixed.cmake
@@ -2,7 +2,7 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-if(PORT MATCHES "sdl2")
+if(PORT MATCHES "sdl")
   set(VCPKG_LIBRARY_LINKAGE dynamic)
   set(VCPKG_CRT_LINKAGE dynamic)
 else()
--- cmake/custom-triplets/x64-windows-mixed.cmake
+++ cmake/custom-triplets/x64-windows-mixed.cmake
@@ -1,11 +1,10 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
 
-set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+set(VCPKG_CMAKE_SYSTEM_NAME Windows)
 
 if(PORT MATCHES "sdl")
   set(VCPKG_LIBRARY_LINKAGE dynamic)
-  set(VCPKG_CRT_LINKAGE dynamic)
 else()
   set(VCPKG_LIBRARY_LINKAGE static)
-  set(VCPKG_CRT_LINKAGE static)
 endif()
