Fix action index calculation for continuous actions (#638)
Co-authored-by: Mark Towers <mark.m.towers@gmail.com>

src/ale/python/env.py
@@ -284,11 +284,11 @@ class AtariEnv(gymnasium.Env, utils.EzPickle):
             x, y = action[0] * np.cos(action[1]), action[0] * np.sin(action[1])
             action_idx = self.map_action_idx(
                 left_center_right=(
-                    -int(x < self.continuous_action_threshold)
+                    -int(x < -self.continuous_action_threshold)
                     + int(x > self.continuous_action_threshold)
                 ),
                 down_center_up=(
-                    -int(y < self.continuous_action_threshold)
+                    -int(y < -self.continuous_action_threshold)
                     + int(y > self.continuous_action_threshold)
                 ),
                 fire=(action[-1] > self.continuous_action_threshold),